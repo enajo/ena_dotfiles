3094c72c57339551711a1d9f04ddf3920b276f4c
enajo <enajohnegbe@gmail.com>
2023-11-12 12:42:59 +0100

    initial

---
+++ install_system.sh
@@ -0,0 +1,5 @@
+sudo apt install curl zsh git
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/romkatv/powerlevel10k.git /home/enajo/.oh-my-zsh/custom/themes/powerlevel10k
+ git config --global user.email "enajohnegbe@gmail.com"
+ git config --global user.name "enajo"
